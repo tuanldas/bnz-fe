c4973b3ada02ed15b2f2f671f5e4739b4704c297
tuanldas <tuanldas@gnail.com>
2025-04-02 15:22:46 +0700

    Khởi tạo dự án

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { fileURLToPath, URL } from 'node:url';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import tailwindcss from 'tailwindcss';
+
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()]
+    }
+  },
+  server: {
+    port: 3000,
+    host: true
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    chunkSizeWarningLimit: 3000
+  }
+});
