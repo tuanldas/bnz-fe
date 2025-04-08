0d1d78feea07ae47a511e5b1f6a574420971b1a3
tuanldas <tuanldas@gnail.com>
2025-04-08 16:05:45 +0700

    Thêm danh sách lớp

---
+++ src/pages/classes/Classes.tsx
@@ -0,0 +1,39 @@
+import {callApiGetStudents} from '@/api/class.tsx';
+import {useQuery} from 'react-query';
+import {Fragment} from 'react';
+
+import {Container} from '@/components/container';
+import {Toolbar, ToolbarHeading, ToolbarPageTitle} from '@/partials/toolbar';
+
+import {useLayout} from '@/providers';
+import {List} from '@/pages/classes/blocks/List.tsx';
+
+const Classes = () => {
+    const {currentLayout} = useLayout();
+    const {data, isLoading, error} = useQuery({
+        queryKey: ['Classes'],
+        queryFn: callApiGetStudents,
+    });
+    console.log(data?.data?.data);
+    return <>
+        <Fragment>
+            {currentLayout?.name === 'demo1-layout' && (
+                <Container>
+                    <Toolbar>
+                        <ToolbarHeading>
+                            <ToolbarPageTitle/>
+                        </ToolbarHeading>
+                    </Toolbar>
+                </Container>
+            )}
+
+            <Container>
+                <div className="grid gap-5 lg:gap-7.5">
+                    <List classes={data?.data?.data}/>
+                </div>
+            </Container>
+        </Fragment>
+    </>;
+};
+
+export {Classes};
---
+++ src/pages/classes/blocks/List.tsx
@@ -0,0 +1,39 @@
+import {Column} from '@tanstack/react-table';
+
+
+interface IColumnFilterProps<TData, TValue> {
+    column: Column<TData, TValue>;
+}
+
+const List = ({classes}: { classes: any }) => {
+    return (
+        <div className="card">
+            <div className="card-table scrollable-x-auto">
+                <table className="table">
+                    <thead>
+                    <tr>
+                        <th className="">ID</th>
+                        <th className="min-w-24">Lớp</th>
+                        <th className="min-w-32">Mô tả</th>
+                        <th className="w-8"></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        classes ? classes.map((column: any) => (
+                                <tr key={column.id}>
+                                    <td className="text-sm text-gray-800 font-normal">{column.id}</td>
+                                    <td className="text-sm text-gray-800 font-normal">{column.name}</td>
+                                    <td className="text-sm text-gray-800 font-normal">{column.description}</td>
+                                </tr>
+                            ))
+                            : null
+                    }
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+};
+
+export {List};
